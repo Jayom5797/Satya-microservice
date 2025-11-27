b0ecad468207f2b3ba9cacd860bfc6eac861ea45
Jayom5797 <jayom5797@gail.com>
2025-11-27 12:56:26 +0530

    Fix worker Redis connection and increase Django timeout

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Start the worker in the background
-python -m app.worker &
+# Start the simple worker in the background
+python -m app.worker_simple &
 
 # Start the API server in the foreground
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
