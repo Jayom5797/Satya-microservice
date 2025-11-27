a33417365129884e3b1e2dc76b80950b1f282906
Jayom5797 <jayom5797@gail.com>
2025-11-27 13:34:45 +0530

    Clean up and optimize for Render deployment

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start the simple worker in the background
-python -m app.worker_simple &
-
-# Start the API server in the foreground
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
